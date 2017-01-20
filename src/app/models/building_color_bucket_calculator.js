eac0925a034289db242ca33dca472e4f14201f13
Sean Connelley <connelley@stamen.com>
2017-01-20 12:10:10 -0500

    Improved BuildingColorBucketCalculator performance
    Prevent duplicate calls to load cartoLayer

--- src/app/models/building_color_bucket_calculator.js
+++ src/app/models/building_color_bucket_calculator.js
@@ -7,39 +7,94 @@ define([
     this.fieldName = fieldName;
     this.buckets = buckets;
     this.colorStops = colorStops;
+
+
+    this.memoized = {};
+    this.memoized.fieldValues = {};
+    this.memoized.colorGradients = {};
+    this.memoized.cartoCSS = {};
+    this.memoized.bucketStops = this.calcBucketStops();
+    this.memoized.gradientStops = this.calcGradientStops();
   };
 
-  BuildingColorBucketCalculator.prototype.toBucketStops = function() {
+
+
+  BuildingColorBucketCalculator.prototype.calcBucketStops = function() {
     var range = this.colorStops,
         buckets = this.buckets,
         rangeCount = _.max([range.length - 1, 1]),
         domain = _.range(0, buckets, buckets / rangeCount).concat(buckets);
+
     return _.map(domain, function(bucket) { return _.max([0, bucket - 1]); });
   };
 
-  BuildingColorBucketCalculator.prototype.toGradientStops = function() {
+
+  BuildingColorBucketCalculator.prototype.calcGradientStops = function() {
     var range = this.colorStops,
         buckets = this.buckets,
         bucketStops = this.toBucketStops(),
         gradientScale = d3.scale.linear().range(range).domain(bucketStops);
+
     return _.map(_.range(buckets), gradientScale);
   };
 
-  BuildingColorBucketCalculator.prototype.toCartoCSS = function() {
+  BuildingColorBucketCalculator.prototype.cartoCSS = function() {
+    if (this.memoized.cartoCSS.hasOwnProperty(this.fieldName)) {
+      return this.memoized.cartoCSS[this.fieldName];
+    }
+
     var stops = this.toGradientStops(),
         fieldName = this.fieldName,
-        fieldValues = this.buildings.pluck(fieldName),
-        gradient = d3.scale.quantile().domain(fieldValues).range(stops);
-    return _.map(stops, function(stop){
+        fieldValues = this.getFieldValues(),
+        gradient = this.colorGradient();
+
+    var css = this.memoized.cartoCSS[this.fieldName] = _.map(stops, function(stop){
       var min = _.min(gradient.invertExtent(stop));
       return "[" + fieldName + ">=" + min + "]{marker-fill:" + stop + "}";
     });
+
+    return css;
+  }
+
+  BuildingColorBucketCalculator.prototype.getFieldValues = function() {
+    if (this.memoized.fieldValues.hasOwnProperty(this.fieldName)) {
+      return this.memoized.fieldValues[this.fieldName];
+    }
+
+    var fieldValues = this.memoized.fieldValues[this.fieldName] = this.buildings.pluck(this.fieldName);
+
+    return fieldValues;
+  };
+
+  BuildingColorBucketCalculator.prototype.colorGradient = function() {
+    if (this.memoized.colorGradients.hasOwnProperty(this.fieldName)) {
+      return this.memoized.colorGradients[this.fieldName];
+    }
+
+    var stops = this.toGradientStops();
+    var fieldValues = this.getFieldValues();
+    var scale = this.memoized.colorGradients[this.fieldName] = d3.scale.quantile().domain(fieldValues).range(stops);
+
+    return scale;
+  }
+
+  // Calculated in constructor
+  BuildingColorBucketCalculator.prototype.toBucketStops = function() {
+    return this.memoized.bucketStops;
+  }
+
+  // Calculated in constructor
+  BuildingColorBucketCalculator.prototype.toGradientStops = function() {
+    return this.memoized.gradientStops;
+  }
+
+  BuildingColorBucketCalculator.prototype.toCartoCSS = function() {
+    return this.cartoCSS();
   };
 
   BuildingColorBucketCalculator.prototype.toColor = function(value) {
-    var stops = this.toGradientStops(),
-        fieldValues = this.buildings.pluck(this.fieldName),
-        gradient = d3.scale.quantile().domain(fieldValues).range(stops);
+    var gradient = this.colorGradient();
+
     return gradient(value);
   };
 
