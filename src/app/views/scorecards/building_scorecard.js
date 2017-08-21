dc8152a3155b9dd47d24262a8ae31a512189d9a7
Sean Connelley <connelley@stamen.com>
2017-08-21 15:40:27 -0700

    Text overlap in scorecard change chart, #99

--- src/app/views/scorecards/building_scorecard.js
+++ src/app/views/scorecards/building_scorecard.js
@@ -792,7 +792,52 @@ define([
           .attr('class', 'building')
           .classed('avg', d => d.isAvg)
           .text(d => d.label);
-    }
+
+
+      var me = this;
+      var prev;
+      label.each(function(d) {
+        if (prev) {
+          var rect1 = me.makeRect(prev);
+          var rect2 = me.makeRect(this);
+          var attempts = 10;
+
+          while(me.collision(rect1, rect2) && attempts > 0) {
+            attempts--;
+            prev.style.top = (rect1.top - 2) + 'px';
+            this.style.top = (rect2.top + 2) + 'px';
+
+            rect1 = me.makeRect(prev);
+            rect2 = me.makeRect(this);
+          }
+
+        }
+        prev = this;
+      });
+    },
+
+    makeRect: function(el) {
+      var t = el.offsetTop;
+      var l = el.offsetLeft;
+
+      return {
+        top: t,
+        right: l + el.offsetWidth,
+        bottom: t + el.offsetHeight,
+        left: l
+      }
+    },
+
+    collision: function(rect1, rect2) {
+      return !(rect1.right < rect2.left ||
+              rect1.left > rect2.right ||
+              rect1.bottom < rect2.top ||
+              rect1.top > rect2.bottom);
+    },
+
+    fixChangeOverlaps: function() {
+
+    },
   });
 
   return BuildingScorecard;
