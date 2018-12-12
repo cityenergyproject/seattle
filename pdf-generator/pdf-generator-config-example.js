178ba72dfbe59a90fca7dc593cf6900a6c76ed5b
Eric Brelsford <ebrelsford@gmail.com>
2018-12-12 12:05:19 -0500

    Keep all config together

--- pdf-generator/pdf-generator-config-example.js
+++ pdf-generator/pdf-generator-config-example.js
@@ -1,5 +1,11 @@
 module.exports = {
   baseUrl: 'http://localhost:8000',
   outputDirectory: 'test',
-  scriptLocation: 'index.js'
+  scriptLocation: 'index.js',
+  email: {
+    cc: ['cc@example.com'],
+    from: 'from@example.com',
+    replyTo: ['reply-to@example.com'],
+    returnPath: 'return-path@example.com',
+  }
 };
