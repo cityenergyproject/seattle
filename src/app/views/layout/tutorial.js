f72267e1b7592f365a39ce01aced4da03ecc65a5
Tom Allnutt <tom@greeninfo.org>
2023-11-30 14:08:17 -0800

    Tutorial updates close #69

--- src/app/views/layout/tutorial.js
+++ src/app/views/layout/tutorial.js
@@ -31,11 +31,25 @@ define([
       const userlng = state.get('lng');
       const userzoom = state.get('zoom');
 
+      // A sequence of steps to run after the tutorial ends, to reset the app state
+      function endTurorial() {
+        // END: zoom back out to the default view and deselect the building
+        $('#back-to-map-link').click();
+        mapview.leafletMap.setView([userlat, userlng], userzoom);
+        state.set({building: null});
+        state.set({selected_buildings: []});
+        mapview.leafletMap.closePopup();
+        mapview.leafletMap.highlightLayer.clearLayers();
+      }
+
+      // This is the main config for the tutorial
 			const driverObj = driver({
 			  showProgress: true,
 			  allowClose: true,
+        onDestroyed: endTurorial,
 			  showButtons: ['next', 'close'],
 			  steps: [
+          // STEP 1
 			    { element: '#search',
 			    	onHighlighted: () => { 
 			    		// make sure we have focus
@@ -58,7 +72,7 @@ define([
               })
 
               $('#total_ghg_emissions').click();                  // select default layer
-              // $('input#year-2021').click();                       // select 2021 which we know works
+              // $('input#year-2021').click();                    // select 2021 which we know works
 			    		let citycenter = state.get('city').get('center');   // map center
 			    		let cityzoom = state.get('city').get('zoom');       // map zoom
 							mapview.leafletMap.setView(citycenter, cityzoom);   // set map xyz
@@ -70,12 +84,13 @@ define([
 			    	},
 			    	popover: { 
 			    		title: 'Search', 
-			    		description: 'Use the Search bar to find a building based on its name, address, or building ID.' 
+			    		description: 'Use the Search bar to find a building based on its name, address, or Seattle OSE building ID.' 
 			    	} 
 			    },
 			    { element: '#map-category-controls', 
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
 			    	popover: { 
+						// STEP 2
 			    		title: 'Filter buildings', 
 			    		description: 'Filter buildings based on criteria like neighborhood, council district or reporting year',
 			  			onNextClick: () => {
@@ -100,9 +115,10 @@ define([
 			    		// which is why we had to add an image of the menu in the previous step
 			    		document.querySelector('.driver-popover').focus();
 			    	},
+            // STEP 3
 			    	popover: { 
 			    		title: 'Filter buildings', 
-			    		description: 'You can also filter buildings by property type' 
+			    		description: 'Users can also filter buildings by property type' 
 			    	},
 			    	onDeselected: () => {
 			    		// delete the menu image
@@ -111,9 +127,10 @@ define([
 			    },
 			    { element: '#total_ghg_emissions', 
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
+            // STEP 4
 			    	popover: { 
 			    		title: 'Map display', 
-			    		description: 'The map defaults to displaying greenhouse gas (GHG) emissions; users can toggle between absolute GHG emissions and GHG intensity (emissions per square foot), as shown next. Move the sliders to filter for the highest or lowest emitting buildings.',
+			    		description: 'The map defaults to displaying greenhouse gas (GHG) emissions; users can toggle between absolute GHG emissions and GHG intensity (GHG emissions per square foot of building floor area). Move the sliders to filter for the highest or lowest emitting buildings.',
 			  			onNextClick: () => {
 			  				// open the next panel
 			  				$('#total_ghg_emissions_intensity').click();
@@ -125,7 +142,7 @@ define([
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
 			    	popover: { 
 			    		title: 'Map display', 
-			    		description: 'You can also visualize buildings by GHG intensity (i.e. emissions per square foot)',
+			    		description: 'YUsers can visualize buildings by GHG intensity in kilograms of carbon dioxide equivalent (CO2e) per square foot.',
 			        onNextClick: () => {
 			          // Collapse the accordion
 			          $('input#category-greenhouse-gas-emissions-expanded').click();
@@ -138,9 +155,10 @@ define([
 		      },
 		      { element: '#map-controls-content--inner',
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
-			    	popover: { 
+			    	popover: {
+              // STEP 6
 			    		title: 'Map display', 
-			    		description: 'Users can choose to display greenhouse gas emissions, energy performance metrics, and property information like square footage. Click on these tabs to minimize or maximize these data options.', 
+			    		description: 'Users can choose to display greenhouse gas emissions, energy performance metrics, and property information like square footage. Click on these headers to minimize or maximize these data options. Click “More Info” to read about each display metric.', 
 				    	onNextClick: () => {
 				    		// Zoom in so that building footprints are shown
 				    		mapview.leafletMap.setView([47.6050418, -122.3299205], 16);
@@ -157,7 +175,8 @@ define([
 		    	},
 			    { element: '#map', 
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
-			    	popover: { 
+			    	popover: {
+              // STEP 7
 			    		title: 'Map display', 
 			    		description: 'Zooming in will display additional detail about the building location and footprints.',
 				    	onNextClick: () => {
@@ -170,17 +189,18 @@ define([
 			    },
 			    { element: '#map', 
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
+            // STEP 8
 			    	popover: { 
 			    		title: 'Map display', 
-			    		description: 'Clicking on a building will display an information snapshot. To view a more detailed, building-specific report, click on "View Building Report".',
+			    		description: 'Clicking on a building shape or dot will display an information pop-up. To view a more detailed, building-specific performance profile, click on “View Building Report”.',
 			    	},
 			    },
 			    { element: 'table.comparables', 
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
 			    	popover: { 
+              // STEP 9
 			    		title: 'Compare buildings', 
-			    		description: 'Buildings that are selected in succession will populate the Building Comparison tab. If you click on Building Comparison, a side by side comparison will expand from the bottom of the screen.',
-			    	
+			    		description: 'Buildings that are selected in succession will populate the Building Comparison tab. If you click on Building Comparison, a side-by-side comparison will expand from the bottom of the screen. Select different display metrics via the left pane',			    	
 				    	onNextClick: () => {
 				    		// Select a building and quickly click "Show Report"
 				    		state.set({building: '357'});
@@ -194,23 +214,34 @@ define([
 			    },
 			    { element: '#building-details-cards-wrapper', 
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
-			    	popover: { 
+			    	popover: {
+              // STEP 10
 			    		title: 'Building performance details', 
-			    		description: 'The customized Building Report displays the building’s EUI and GHG Intensity for the selected reporting year and frames them in the context of their peers. The report also displays the energy consumption and GHG emission breakdown by fuel type (e.g. electricity, natural gas, district steam).',
+			    		description: 'The customized Building Report displays the building’s Site EUI and GHG Intensity for the selected reporting year and frames them in the context of their peers. The report also displays the energy consumption and GHG emission breakdown by fuel type (e.g. electricity, natural gas, district steam).',
 			    	},
 			    },
 			    { element: '#change-chart', 
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
+            // STEP 11
 			    	popover: { 
 			    		title: 'Building performance trends', 
 			    		description: 'The report displays performance trends of the same building over time against a typical building of the same property type.',
 			    	},
 			    },
-			    { element: '#performance-standard-chart', 
+          { element: '#compare-shift-chart', 
 			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
+            // STEP 12 (new)
 			    	popover: { 
 			    		title: 'Building performance standard', 
 			    		description: 'For commercial buildings 50,000 SF and larger, the report displays current energy performance versus an approximate Washington Clean Buildings Performance Standard EUI target.',
+			    	},
+			    },
+			    { element: '#performance-standard-chart', 
+			    	onHighlighted: () => { document.querySelector('.driver-popover').focus(); },
+			    	// STEP 13
+            popover: { 
+			    		title: 'Building performance standard', 
+			    		description: 'Users can also find utility incentive opportunities at the bottom of the report.',
 			    	},
 			    },
 			    { element: '#links', 
@@ -218,16 +249,7 @@ define([
 			    	popover: { 
 			    		title: 'Utility incentives', 
 			    		description: 'Users can also find utility incentive opportunities at the bottom of the report.',
-			    	},
-			    	onDeselected: () => {
-			    		// END: zoom back out to the default view and deselect the building
-			    		$('#back-to-map-link').click();
-							mapview.leafletMap.setView([userlat, userlng], userzoom);
-							state.set({building: null});
-							state.set({selected_buildings: []});
-							mapview.leafletMap.closePopup();
-							mapview.leafletMap.highlightLayer.clearLayers();
-			    	}, 
+			    	}
 			    },
 			  ]
 	  	});
